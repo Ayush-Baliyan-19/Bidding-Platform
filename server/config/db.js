6208634e1ed1ee92dd8b13bab56bc2177c2404ec
--get <ayushbaliyan05@gmail.com>
2025-05-31 23:15:43 +0530

    fix: reject unauthorised

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
